08385f52b2ad5f351a809f6b2b03a5ce9b87dc34
沈伟 <330589465@qq.com>
2021-03-19 17:25:30 +0800

    添加项目文件。

---
+++ Stargazer.Extensions.Newtonsoft.Json.StrictContract.Tests/StrictContractResolverTests.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using Stargazer.Extensions.Newtonsoft.Json.Strict;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable enable
+
+namespace Stargazer.Extensions.Newtonsoft.Json.Strict
+{
+    [TestFixture()]
+    public class StrictContractResolverTests
+    {
+        private JsonSerializerSettings _settings = new() { ContractResolver = new StrictContractResolver() };
+
+        public class ConstructorHasParametersClass
+        {
+            public ConstructorHasParametersClass(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+
+            public string Name { get; }
+
+            public int Age { get; }
+
+            public string Gender { get; set; } = "Unknown";
+        }
+
+        [Test]
+        public void MissingConstructorParametersTest()
+        {
+            Assert.Throws<JsonSerializationException>(() =>
+                JsonConvert.DeserializeObject<ConstructorHasParametersClass>("{\"name\":\"bob\"}", _settings),
+            "");
+        }
+
+        [Test]
+        public void SetNonnullablePropertyAsNullTest()
+        {
+            Assert.Throws<JsonSerializationException>(() =>
+                JsonConvert.DeserializeObject<ConstructorHasParametersClass>("{\"name\":\"bob\",\"age\":18,\"gender\":null}", _settings),
+            "");
+        }
+    }
+}
---
+++ Stargazer.Extensions.Newtonsoft.Json.StrictContract/StrictContractResolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Namotion.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Stargazer.Extensions.Newtonsoft.Json.Strict
+{
+    public class StrictContractResolver : CamelCasePropertyNamesContractResolver
+    {
+        protected override JsonObjectContract CreateObjectContract(Type objectType)
+        {
+            var contract = base.CreateObjectContract(objectType);
+            foreach(var prop in contract.Properties)
+            {
+                if(prop.DeclaringType!.GetMember(prop.UnderlyingName!).First().ToContextualMember().Nullability is Nullability.NotNullable)
+                {
+                    prop.Required = prop.Required switch
+                    {
+                        Required.Default => Required.DisallowNull,
+                        Required.AllowNull => Required.Always,
+                        _ => prop.Required
+                    };
+                }
+
+                if(contract.CreatorParameters.Any(x => string.Equals(x.UnderlyingName, prop.UnderlyingName, StringComparison.OrdinalIgnoreCase)))
+                {
+                    prop.Required = prop.Required switch
+                    {
+                        Required.Default => Required.AllowNull,
+                        Required.DisallowNull => Required.Always,
+                        _ => prop.Required
+                    };
+                }
+            }
+            return contract;
+        }
+    }
+}
